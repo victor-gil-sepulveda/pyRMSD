77676a20486c9d15d178c7c383e0621467a0bf72
victor-gil-sepulveda <victor.gil.sepulveda@gmail.com>
2013-03-07 18:38:19 +0100

    Added again the available calculators file.

---
+++ pyRMSD/availableCalculators.py
@@ -0,0 +1,14 @@
+
+def availableCalculators():
+    return {
+            "KABSCH_PYTHON_CALCULATOR":-1, 
+            "KABSCH_SERIAL_CALCULATOR": 0, 
+            "KABSCH_OMP_CALCULATOR":1, 
+            #"KABSCH_CUDA_CALCULATOR":2, 
+            "QTRFIT_SERIAL_CALCULATOR":3,
+            "QTRFIT_OMP_CALCULATOR":4,
+            #"QTRFIT_CUDA_CALCULATOR":5,
+            "QCP_SERIAL_CALCULATOR":6,
+            "QCP_OMP_CALCULATOR":7,
+            #"QCP_CUDA_CALCULATOR":8
+    }
